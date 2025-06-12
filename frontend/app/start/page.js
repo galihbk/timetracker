98a0cbd2d72f04fc41e81485e81bcfe61e0f290c
Galih Bagaskoro <89682863+galihbk@users.noreply.github.com>
2025-06-13 02:56:55 +0700

    Update page.js

--- frontend/app/start/page.js
+++ frontend/app/start/page.js
@@ -24,7 +24,7 @@ export default function StartPage() {
     const waktu = new Date().toLocaleString("sv-SE").replace(" ", "T");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/bib/start", {
+      const res = await axios.post("http://34.9.34.33:5000/api/bib/start", {
         waktu,
       });
 
