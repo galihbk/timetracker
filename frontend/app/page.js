a0ccc529ce073fd187841941fdf90288e18bdee6
Galih Bagaskoro <89682863+galihbk@users.noreply.github.com>
2025-06-13 21:29:10 +0700

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -11,14 +11,14 @@ export default function LeaderboardPage() {
   useEffect(() => {
     fetchLeaderboard();
 
-    const socket = io("http://35.232.72.141:5000");
+    const socket = io("https://result.bibsport.id");
     socket.on("new_bib", () => fetchLeaderboard());
     return () => socket.disconnect();
   }, []);
 
   const fetchLeaderboard = async () => {
     try {
-      const res = await axios.get("http://35.232.72.141:5000/api/bib/leaderboard");
+      const res = await axios.get("https://result.bibsport.id/api/bib/leaderboard");
       const { lokasi, data } = res.data;
 
       // Tambahkan awalan M atau F berdasarkan gender
